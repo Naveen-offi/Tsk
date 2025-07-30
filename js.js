64bfa5a8969fc819e9079cb7854cbfcffd37b67a
Naveen <naveenking2002@gmail.com>
2025-07-30 10:44:19 +0530

    Uploaded01
    Uploaded

---
+++ js.js
@@ -0,0 +1,20 @@
+// function shop(){
+    // window.open("https://wa.me/+919688880848", "_blank");
+        // Whatsapp msg
+// }
+    
+
+function shop(id, title){
+    var id = document.getElementById(id).innerHTML;
+    var title = document.getElementById(title).innerHTML; 
+    var link = "https://wa.me/+919688880848?text=" + "I Need this product. Product Name :  *" + title + "*. Product id: *"+ id + "*"; 
+    window.open(link, "_blank");
+    // console.log("I Need this product. Product Name : " + title + ". Product id: "+ id);
+}
+
+function contact(){
+    var cus_name = prompt("Enter Your Name: ");
+    var link = "https://wa.me/+919688880848?text=" + "Hi, *Senthil Kumar*. I am "+ cus_name + ". I need Your *SERVICE*"; 
+    window.open(link, "_blank");
+    // console.log("Hi, Senthil Kumar. I am "+ cus_name + " I need your ***SERVICE***");
+}
---
+++ js1.js
@@ -0,0 +1,27 @@
+
+// // var a = "https://wa.me/+919566406178?text="
+// // // var b = "hii"
+// var b = document.getElementById("addit").innerHTML;
+// // // alert(b);
+// console.log(b)
+
+// // // var c = a+b
+
+// // // console.log(c)
+
+// function shop(){
+//     var id = document.getElementById(id).innerHTML;
+//     var title = document.getElementById(title).innerHTML;
+
+//     var link = "https://wa.me/+919688880848?text="
+    
+//     var final = link + "I_need_this" + id + title
+
+//     console.log(final)
+
+//     // window.open(final, "_blank");
+// }
+
+// var b = "id1"
+// var a = document.getElementById(b).outerHTML;
+// console.log(a);
